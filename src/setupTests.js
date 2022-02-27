4021e1a3f416a07e858cab1224be7329ec7269e4
jaimeperezpedra <jaimeperezpedra@gmail.com>
2022-02-27 19:19:20 +0100

    fix the date to today in order to fix the tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import Mockdate from "mockdate"
+
+const mockdateInit = 1645985898285 // 2020-02-04T02:22:40
+
+global.beforeEach(() => {
+  Mockdate.set(mockdateInit)
+});
